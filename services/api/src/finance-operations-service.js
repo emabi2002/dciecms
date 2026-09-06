1d2f5b652d9476218fda44f7e8ab36f92f0b346b
emabi2002 <emabi2002@gmail.com>
2026-09-06 16:05:09 +1000

    feat: add R3 finance operations service boundary

---
+++ services/api/src/finance-operations-service.js
@@ -0,0 +1,6 @@
+'use strict';
+const { PersistentDciecmsService } = require('./persistent-dciecms-service');
+
+class FinanceOperationsService extends PersistentDciecmsService {}
+
+module.exports = { FinanceOperationsService };
