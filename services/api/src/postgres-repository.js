df9967a4c4a91b8ddf8227a6d13f89f8725f03ac
emabi2002 <emabi2002@gmail.com>
2026-09-07 13:08:40 +1000

    feat: install verified gateway repository extension

--- services/api/src/postgres-repository.js
+++ services/api/src/postgres-repository.js
@@ -2,7 +2,9 @@
 
 const core = require('./postgres-repository-core');
 const { installSecureDocumentRepository } = require('./postgres-secure-document-repository');
+const { installPaymentGatewayRepository } = require('./postgres-payment-gateway-repository');
 
 installSecureDocumentRepository(core.PostgresRepository);
+installPaymentGatewayRepository(core.PostgresRepository);
 
 module.exports = core;
