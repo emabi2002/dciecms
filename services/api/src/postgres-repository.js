7e2f7c493b9c1a1e8d5fd0a90cd3f193ae90d7a4
emabi2002 <emabi2002@gmail.com>
2026-09-07 10:24:28 +1000

    feat: install payment integration repository methods

--- services/api/src/postgres-repository.js
+++ services/api/src/postgres-repository.js
@@ -2,7 +2,9 @@
 
 const core = require('./postgres-repository-core');
 const { installSecureDocumentRepository } = require('./postgres-secure-document-repository');
+const { installPaymentIntegrationRepository } = require('./postgres-payment-integration-repository');
 
 installSecureDocumentRepository(core.PostgresRepository);
+installPaymentIntegrationRepository(core.PostgresRepository);
 
 module.exports = core;
