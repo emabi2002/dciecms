bf2a031be9deee50c7105ec4ce62959d45de18e8
emabi2002 <emabi2002@gmail.com>
2026-09-08 00:12:20 +1000

    feat: attach R3 finance completion facade

--- services/api/src/persistent-dciecms-service.js
+++ services/api/src/persistent-dciecms-service.js
@@ -4,6 +4,7 @@ const core = require('./persistent-dciecms-service-core');
 const { installSecureDocumentFacade } = require('./secure-document-facade');
 const { installPaymentIntegrationFacade } = require('./payment-integration-facade');
 const { installCaseLifecycleRecordsFacade } = require('./case-lifecycle-records-facade');
+const { installFinanceCompletionFacade } = require('./finance-completion-facade');
 
 class PersistentDciecmsService extends core.PersistentDciecmsService {
   constructor(options = {}) {
@@ -11,11 +12,13 @@ class PersistentDciecmsService extends core.PersistentDciecmsService {
     this.secureDocuments = options.secureDocumentService || null;
     this.paymentIntegration = options.paymentIntegrationService || null;
     this.caseLifecycleRecords = options.caseLifecycleRecordsService || null;
+    this.financeCompletion = options.financeCompletionService || null;
   }
 }
 
 installSecureDocumentFacade(PersistentDciecmsService);
 installPaymentIntegrationFacade(PersistentDciecmsService);
 installCaseLifecycleRecordsFacade(PersistentDciecmsService);
+installFinanceCompletionFacade(PersistentDciecmsService);
 
 module.exports = { ...core, PersistentDciecmsService };
