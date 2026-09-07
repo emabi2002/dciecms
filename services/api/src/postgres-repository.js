011404deca147a4afd1c59216c72fd8625b5c7e8
emabi2002 <emabi2002@gmail.com>
2026-09-07 12:37:05 +1000

    feat: install payment callback repository controls

--- services/api/src/postgres-repository.js
+++ services/api/src/postgres-repository.js
@@ -2,7 +2,9 @@
 
 const core = require('./postgres-repository-core');
 const { installSecureDocumentRepository } = require('./postgres-secure-document-repository');
+const { installPaymentCallbackRepository } = require('./postgres-payment-callback-repository');
 
 installSecureDocumentRepository(core.PostgresRepository);
+installPaymentCallbackRepository(core.PostgresRepository);
 
 module.exports = core;
