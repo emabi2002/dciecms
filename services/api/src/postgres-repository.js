c1bda95f7d1324d87dc1fee3e8f4a94093e2bc78
emabi2002 <emabi2002@gmail.com>
2026-09-08 01:23:29 +1000

    feat: install R3 finance workbench repository

--- services/api/src/postgres-repository.js
+++ services/api/src/postgres-repository.js
@@ -5,10 +5,12 @@ const { installSecureDocumentRepository } = require('./postgres-secure-document-
 const { installPaymentIntegrationRepository } = require('./postgres-payment-integration-repository');
 const { installCaseLifecycleRecordsRepository } = require('./postgres-case-lifecycle-records-repository');
 const { installFinanceCompletionRepository } = require('./postgres-finance-completion-repository');
+const { installR3FinanceWorkbenchRepository } = require('./postgres-r3-finance-workbench-repository');
 
 installSecureDocumentRepository(core.PostgresRepository);
 installPaymentIntegrationRepository(core.PostgresRepository);
 installCaseLifecycleRecordsRepository(core.PostgresRepository);
 installFinanceCompletionRepository(core.PostgresRepository);
+installR3FinanceWorkbenchRepository(core.PostgresRepository);
 
 module.exports = core;
